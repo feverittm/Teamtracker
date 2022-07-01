8ce8acda0ebee98a4937ef09a59e00b7ea076819
Floyd Moore <floyd.moore@hp.com>
2022-07-01 12:59:12 -0700

    add the database connections in sqlite

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'c219cad489c95a9ddfdda573af021c0f'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
